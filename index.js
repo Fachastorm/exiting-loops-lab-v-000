009fe8875bebe152d9eee799630be68612f1b794
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-21 03:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,4 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 
-function firstNotEqual(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    debugger
-    if (array[i] !== value) {
-      return array[i]
-    }
-  }
-}
+
