59d165c1265d5848968c24379e5e980c267ea561
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-21 03:51:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i =0; i < array.length; i++) {
-    if
+    if(array[i] == stopValue)
+    break 
   }
 
 }
 
 
-
+function firstNotEqual(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    debugger
+    if (array[i] !== value) {
+      return array[i]
+    }
+  }
+}
